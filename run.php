2a2c426242f235b27c56c1f3195ada6dfdbd5a26
rafaqueque <c.rafael.s.albuquerque@gmail.com>
2013-03-29 01:38:02 +0000

    quick fix and improvement

--- run.php
+++ run.php
@@ -2,7 +2,7 @@
 
   if (!$argv[1])
   {
-    die("* hey, we need a file.");
+    die("* ERROR * syntax is: php ".basename(__FILE__)." /path/to/tweets.csv \n");
   }
   else
   {
@@ -16,14 +16,26 @@
       {
         while (($data = fgetcsv($handle, 2000, ',','"')) !== FALSE) 
         {
-          if ($data[0]) $stats['tweets'] += 1;
-          if ($data[2]) $stats['replies'] += 1;
-          if ($data[3]) $stats['retweets'] += 1;
+          if ($data[0]) 
+            $stats['tweets'] += 1;
+
+          if ($data[2]) 
+            $stats['replies'] += 1;
+
+          if ($data[3]) 
+            $stats['retweets'] += 1;
+
+          $stats['first_tweet'] = $data;
         }
       }
 
+      echo "here's your (raw) data: \n";
       print_r($stats);
 
+    }
+    else
+    {
+      die("* ERROR * specified path (".$argv[1].") doesn't exist \n");
     }
 
   }
